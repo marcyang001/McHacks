e90a1b1cd710e1733160ad6d8679fd8935cde701
sangmoonhwang <ghkdtkdans@gmail.com>
2016-02-20 15:29:12 -0500

    More advanced example: guestbook

--- src/main/java/com/example/appengine/helloworld/HelloServlet.java
+++ src/main/java/com/example/appengine/helloworld/HelloServlet.java
@@ -17,20 +17,38 @@
 package com.example.appengine.helloworld;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.Properties;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
 // [START example]
 @SuppressWarnings("serial")
 public class HelloServlet extends HttpServlet {
-
   @Override
-  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    PrintWriter out = resp.getWriter();
-    out.println("Hello, world");
+  public void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException {
+    if (req.getParameter("testing") == null) {
+      resp.setContentType("text/plain");
+      resp.getWriter().println("Hello, this is a testing servlet. \n\n");
+      Properties p = System.getProperties();
+      p.list(resp.getWriter());
+
+    } else {
+      UserService userService = UserServiceFactory.getUserService();
+      User currentUser = userService.getCurrentUser();
+
+      if (currentUser != null) {
+        resp.setContentType("text/plain");
+        resp.getWriter().println("Hello, " + currentUser.getNickname());
+      } else {
+        resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
+      }
+    }
   }
 }
-// [END example]
